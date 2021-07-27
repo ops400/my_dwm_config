eb136596c23322c3828490a283ca5906fe2d6d39
OPS 400 <79289232+ops400@users.noreply.github.com>
2021-07-26 21:25:13 -0300

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -2,17 +2,17 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 9;        /* gaps between windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Mono:size=10" };
-static const char dmenufont[]       = "Ubuntu Mono:size=10";
-static const char col_gray1[]       = "#282828";
+static const char *fonts[]          = { "terminus:size=10" };
+static const char dmenufont[]       = "terminus:size=10";
+static const char col_gray1[]       = "#151515";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#282828";
-static const char col_cyan[]        = "#73cef4";
+static const char col_gray4[]       = "#151515";
+static const char col_cyan[]        = "#597bc5";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
